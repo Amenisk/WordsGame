f812622037c328fe08eb338785b6094b0363de05
Камиль Зиятдинов <k.ziatdinov2003@gmail.com>
2023-01-31 11:01:46 +0300

    Chaned scenes and added scriptHolder

--- Assets/Scripts/AnswerPanel.cs
+++ Assets/Scripts/AnswerPanel.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AnswerPanel : MonoBehaviour
 {
+    List<string> _list = new List<string>();
+
     [SerializeField] private Transform _lettersPanel;
     [SerializeField] private GameObject _lettersBtnPrefab;
+    [SerializeField] private GameObject _question;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 30; i++)
+        string word = words[Random.RandomRange(0, words.Length)];
+        for (int i = 0; i < word.Length; i++)
         {
             var btn = Instantiate(_lettersBtnPrefab, _lettersPanel);
-            btn.GetComponentInChildren<Text>().text = Random.RandomRange(10, 100).ToString();
+            btn.GetComponentInChildren<Text>().text = "";
         }
     }
 
---
+++ GameCore/Classes/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCore.Classes
+{
+    public class Question
+    {
+        public string QuestionText { get; private set; }
+        public string AnswerText { get; private set; }
+
+        public Question(string questionText, string answerText)
+        {
+            QuestionText = questionText;
+            AnswerText = answerText;
+        }
+    }
+}
